203b015783f54e336e02572a1ada2d7c425a86c4
Kalen Davison <davison@wustl.edu>
2018-02-02 16:56:33 -0600

    ChoGains added, changed last line of function and the output part of the function

--- Problem Set 2.R
+++ Problem Set 2.R
@@ -1,5 +1,6 @@
+input <- c(1:100)
 
-method <- function(input){
+Leemis <- function(input){
   temp <- as.numeric(substr(input, 1, 1))
   temp.table <- as.data.frame(table(temp))
   freq <- temp.table$Freq/sum(temp.table$Freq)
@@ -11,5 +12,28 @@ method <- function(input){
       }
     }
   }
-  return((out))
+  return(out)
 }
+
+Leemis(input)
+
+
+ChoGains <- function(input){
+  temp <- as.numeric(substr(input, 1, 1))
+  temp.table <- as.data.frame(table(temp))
+  freq <- temp.table$Freq/sum(temp.table$Freq)
+  out <- numeric()
+  for(j in 1:length(levels(temp.table$temp))){
+    for(i in 1:9){
+      if(as.numeric(levels(temp.table$temp)[j])==i){
+        out[i] <- (freq[j] - log10(1+1/i))^2
+      }
+    }
+  }
+  return(out)
+}
+
+sqrt(sum(ChoGains(input)))
+
+
+
