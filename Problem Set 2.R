3992456f9b10d39f0142bf2a63762d8202ef1afe
Kalen Davison <davison@wustl.edu>
2018-02-02 16:39:30 -0600

    Leemis

---
+++ Problem Set 2.R
@@ -0,0 +1,15 @@
+
+method <- function(input){
+  temp <- as.numeric(substr(input, 1, 1))
+  temp.table <- as.data.frame(table(temp))
+  freq <- temp.table$Freq/sum(temp.table$Freq)
+  out <- numeric()
+  for(j in 1:length(levels(temp.table$temp))){
+    for(i in 1:9){
+      if(as.numeric(levels(temp.table$temp)[j])==i){
+        out[i] <- freq[j] - log10(1+1/i)
+      }
+    }
+  }
+  return((out))
+}
